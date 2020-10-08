69d4fbe36e5248c93d06107b43b37c24335fa912
HAVET <vhavet@gmail.com>
2020-10-08 18:58:54 +0200

    integration loader before page home

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,10 @@
 
-
+window.addEventListener("load", function() {
+  document.querySelector(".preloader").classList.add("opacity-0");
+  setTimeout(function() {
+    document.querySelector(".preloader").style.display="none";
+  }, 1000)
+})
 
 // Portfolio Item Filter
 
